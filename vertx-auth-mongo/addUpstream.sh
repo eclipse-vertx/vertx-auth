42ec2b3c716b66b7838291a0d0f3f6eee4b9e8d5
remi128 <mremme@braintags.de>
2015-06-02 20:15:08 +0200

    removed scripts; formatting of pom

--- vertx-auth-mongo/addUpstream.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-git remote add upstream https://github.com/vert-x3/vertx-auth.git
-git remote -v
--- vertx-auth-mongo/updateGit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# SYNCHRONISIEREN DES LOKALEN REPO:
-git fetch upstream  # ( holt Änderungen von Upstream Repo in Index ) 
-git checkout master # ( switch auf master )
-
-# git merge upstream/master # ( mergen in lokales Repo )
-
-git status
