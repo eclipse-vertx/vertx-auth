35920740a489a59253e577603e39dc9d0f71e29e
Julien Viet <julien@julienviet.com>
2015-12-01 09:06:32 +0100

    Remove AuthOptions create static method

--- vertx-auth-common/src/main/java/io/vertx/ext/auth/AuthOptions.java
+++ vertx-auth-common/src/main/java/io/vertx/ext/auth/AuthOptions.java
@@ -17,10 +17,6 @@ package io.vertx.ext.auth;
 
 import io.vertx.codegen.annotations.DataObject;
 import io.vertx.core.Vertx;
-import io.vertx.core.VertxException;
-import io.vertx.core.json.JsonObject;
-
-import java.lang.reflect.Constructor;
 
 /**
  * A common base object for authentication options.<p>
@@ -29,45 +25,6 @@ import java.lang.reflect.Constructor;
  */
 @DataObject
 public interface AuthOptions {
-
-  /**
-   * Create the auth options from a json value, the implementation makes a lookup on the {@literal provider}
-   * property of the json object and returns the corresponding class.
-   *
-   * @param json the json value
-   * @return the auth provider
-   */
-  static AuthOptions create(JsonObject json) {
-
-    String provider = json.getString("provider", "");
-    String impl;
-    switch (provider) {
-      case "shiro":
-        impl = "io.vertx.ext.auth.shiro.ShiroAuthOptions";
-        break;
-      case "jdbc":
-        impl = "io.vertx.ext.auth.jdbc.JDBCAuthOptions";
-        break;
-      case "mongo":
-        impl = "io.vertx.ext.auth.mongo.MongoAuthOptions";
-        break;
-      default:
-        throw new IllegalArgumentException("Invalid auth provider: " + provider);
-    }
-
-    try {
-      ClassLoader cl = Thread.currentThread().getContextClassLoader();
-      Class<?> optionsClass = cl.loadClass(impl);
-      Constructor<?> ctor = optionsClass.getConstructor(JsonObject.class);
-      return (AuthOptions) ctor.newInstance(json);
-    } catch (ClassNotFoundException e) {
-      throw new VertxException("Provider class not found " + impl + " / check your classpath");
-    } catch(InstantiationException e) {
-      throw new VertxException("Cannot create " + provider +" options", e.getCause());
-    } catch (Exception e) {
-      throw new VertxException("Cannot create " + provider + " options" + provider, e);
-    }
-  }
 
   AuthOptions clone();
 
