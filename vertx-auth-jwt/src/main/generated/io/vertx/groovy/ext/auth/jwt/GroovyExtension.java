135f3cb03236fef732fb2a0d9bf8011d61fb5df8
Julien Viet <julien@julienviet.com>
2016-11-04 15:02:02 +0100

    Regen

--- vertx-auth-jwt/src/main/generated/io/vertx/groovy/ext/auth/jwt/GroovyExtension.java
+++ vertx-auth-jwt/src/main/generated/io/vertx/groovy/ext/auth/jwt/GroovyExtension.java
@@ -1,7 +1,7 @@
 package io.vertx.groovy.ext.auth.jwt;
 public class GroovyExtension {
   public static java.lang.String generateToken(io.vertx.ext.auth.jwt.JWTAuth j_receiver, java.util.Map<String, Object> claims, java.util.Map<String, Object> options) {
-    return j_receiver.generateToken(claims != null ? io.vertx.lang.groovy.RetroCompatExtension.toJsonObject(claims) : null,
-      options != null ? new io.vertx.ext.auth.jwt.JWTOptions(io.vertx.lang.groovy.RetroCompatExtension.toJsonObject(options)) : null);
+    return j_receiver.generateToken(claims != null ? io.vertx.lang.groovy.ConversionHelper.toJsonObject(claims) : null,
+      options != null ? new io.vertx.ext.auth.jwt.JWTOptions(io.vertx.lang.groovy.ConversionHelper.toJsonObject(options)) : null);
   }
 }
