08afafad79deeef13f111f01c4d524fa8b24cd0b
Julien Viet <julien@julienviet.com>
2017-01-16 13:57:24 +0100

    Generated code

---
+++ vertx-auth-oauth2/src/main/generated/io/vertx/ext/auth/oauth2/OAuth2ClientOptionsConverter.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2014 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.vertx.ext.auth.oauth2;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+
+/**
+ * Converter for {@link io.vertx.ext.auth.oauth2.OAuth2ClientOptions}.
+ *
+ * NOTE: This class has been automatically generated from the {@link io.vertx.ext.auth.oauth2.OAuth2ClientOptions} original class using Vert.x codegen.
+ */
+public class OAuth2ClientOptionsConverter {
+
+  public static void fromJson(JsonObject json, OAuth2ClientOptions obj) {
+    if (json.getValue("authorizationPath") instanceof String) {
+      obj.setAuthorizationPath((String)json.getValue("authorizationPath"));
+    }
+    if (json.getValue("clientID") instanceof String) {
+      obj.setClientID((String)json.getValue("clientID"));
+    }
+    if (json.getValue("clientSecret") instanceof String) {
+      obj.setClientSecret((String)json.getValue("clientSecret"));
+    }
+    if (json.getValue("clientSecretParameterName") instanceof String) {
+      obj.setClientSecretParameterName((String)json.getValue("clientSecretParameterName"));
+    }
+    if (json.getValue("extraParameters") instanceof JsonObject) {
+      obj.setExtraParameters(((JsonObject)json.getValue("extraParameters")).copy());
+    }
+    if (json.getValue("headers") instanceof JsonObject) {
+      obj.setHeaders(((JsonObject)json.getValue("headers")).copy());
+    }
+    if (json.getValue("introspectionPath") instanceof String) {
+      obj.setIntrospectionPath((String)json.getValue("introspectionPath"));
+    }
+    if (json.getValue("jwtToken") instanceof Boolean) {
+      obj.setJwtToken((Boolean)json.getValue("jwtToken"));
+    }
+    if (json.getValue("logoutPath") instanceof String) {
+      obj.setLogoutPath((String)json.getValue("logoutPath"));
+    }
+    if (json.getValue("privateKey") instanceof String) {
+      obj.setPrivateKey((String)json.getValue("privateKey"));
+    }
+    if (json.getValue("publicKey") instanceof String) {
+      obj.setPublicKey((String)json.getValue("publicKey"));
+    }
+    if (json.getValue("revocationPath") instanceof String) {
+      obj.setRevocationPath((String)json.getValue("revocationPath"));
+    }
+    if (json.getValue("scopeSeparator") instanceof String) {
+      obj.setScopeSeparator((String)json.getValue("scopeSeparator"));
+    }
+    if (json.getValue("site") instanceof String) {
+      obj.setSite((String)json.getValue("site"));
+    }
+    if (json.getValue("tokenPath") instanceof String) {
+      obj.setTokenPath((String)json.getValue("tokenPath"));
+    }
+    if (json.getValue("useBasicAuthorizationHeader") instanceof Boolean) {
+      obj.setUseBasicAuthorizationHeader((Boolean)json.getValue("useBasicAuthorizationHeader"));
+    }
+    if (json.getValue("userAgent") instanceof String) {
+      obj.setUserAgent((String)json.getValue("userAgent"));
+    }
+    if (json.getValue("userInfoPath") instanceof String) {
+      obj.setUserInfoPath((String)json.getValue("userInfoPath"));
+    }
+  }
+
+  public static void toJson(OAuth2ClientOptions obj, JsonObject json) {
+    if (obj.getAuthorizationPath() != null) {
+      json.put("authorizationPath", obj.getAuthorizationPath());
+    }
+    if (obj.getClientID() != null) {
+      json.put("clientID", obj.getClientID());
+    }
+    if (obj.getClientSecret() != null) {
+      json.put("clientSecret", obj.getClientSecret());
+    }
+    if (obj.getClientSecretParameterName() != null) {
+      json.put("clientSecretParameterName", obj.getClientSecretParameterName());
+    }
+    if (obj.getExtraParameters() != null) {
+      json.put("extraParameters", obj.getExtraParameters());
+    }
+    if (obj.getHeaders() != null) {
+      json.put("headers", obj.getHeaders());
+    }
+    if (obj.getIntrospectionPath() != null) {
+      json.put("introspectionPath", obj.getIntrospectionPath());
+    }
+    json.put("jwtToken", obj.isJwtToken());
+    if (obj.getLogoutPath() != null) {
+      json.put("logoutPath", obj.getLogoutPath());
+    }
+    if (obj.getPrivateKey() != null) {
+      json.put("privateKey", obj.getPrivateKey());
+    }
+    if (obj.getPublicKey() != null) {
+      json.put("publicKey", obj.getPublicKey());
+    }
+    if (obj.getRevocationPath() != null) {
+      json.put("revocationPath", obj.getRevocationPath());
+    }
+    if (obj.getScopeSeparator() != null) {
+      json.put("scopeSeparator", obj.getScopeSeparator());
+    }
+    if (obj.getSite() != null) {
+      json.put("site", obj.getSite());
+    }
+    if (obj.getTokenPath() != null) {
+      json.put("tokenPath", obj.getTokenPath());
+    }
+    json.put("useBasicAuthorizationHeader", obj.isUseBasicAuthorizationHeader());
+    if (obj.getUserAgent() != null) {
+      json.put("userAgent", obj.getUserAgent());
+    }
+    if (obj.getUserInfoPath() != null) {
+      json.put("userInfoPath", obj.getUserInfoPath());
+    }
+  }
+}
---
+++ vertx-auth-oauth2/src/main/resources/vertx-auth-oauth2-js/keycloak_auth.js
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2014 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/** @module vertx-auth-oauth2-js/keycloak_auth */
+var utils = require('vertx-js/util/utils');
+var Vertx = require('vertx-js/vertx');
+var OAuth2Auth = require('vertx-auth-oauth2-js/o_auth2_auth');
+
+var io = Packages.io;
+var JsonObject = io.vertx.core.json.JsonObject;
+var JKeycloakAuth = io.vertx.ext.auth.oauth2.providers.KeycloakAuth;
+var HttpClientOptions = io.vertx.core.http.HttpClientOptions;
+
+/**
+
+ @class
+*/
+var KeycloakAuth = function(j_val) {
+
+  var j_keycloakAuth = j_val;
+  var that = this;
+
+  // A reference to the underlying Java delegate
+  // NOTE! This is an internal API and must not be used in user code.
+  // If you rely on this property your code is likely to break if we change it / remove it without warning.
+  this._jdel = j_keycloakAuth;
+};
+
+KeycloakAuth._jclass = utils.getJavaClass("io.vertx.ext.auth.oauth2.providers.KeycloakAuth");
+KeycloakAuth._jtype = {
+  accept: function(obj) {
+    return KeycloakAuth._jclass.isInstance(obj._jdel);
+  },
+  wrap: function(jdel) {
+    var obj = Object.create(KeycloakAuth.prototype, {});
+    KeycloakAuth.apply(obj, arguments);
+    return obj;
+  },
+  unwrap: function(obj) {
+    return obj._jdel;
+  }
+};
+KeycloakAuth._create = function(jdel) {
+  var obj = Object.create(KeycloakAuth.prototype, {});
+  KeycloakAuth.apply(obj, arguments);
+  return obj;
+}
+/**
+ Create a OAuth2Auth provider for Keycloak
+
+ @memberof module:vertx-auth-oauth2-js/keycloak_auth
+ @param vertx {Vertx} 
+ @param flow {Object} the oauth2 flow to use 
+ @param config {Object} the json config file exported from Keycloak admin console 
+ @param httpClientOptions {Object} custom http client options 
+ @return {OAuth2Auth}
+ */
+KeycloakAuth.create = function() {
+  var __args = arguments;
+  if (__args.length === 2 && typeof __args[0] === 'object' && __args[0]._jdel && (typeof __args[1] === 'object' && __args[1] != null)) {
+    return utils.convReturnVertxGen(OAuth2Auth, JKeycloakAuth["create(io.vertx.core.Vertx,io.vertx.core.json.JsonObject)"](__args[0]._jdel, utils.convParamJsonObject(__args[1])));
+  }else if (__args.length === 3 && typeof __args[0] === 'object' && __args[0]._jdel && typeof __args[1] === 'string' && (typeof __args[2] === 'object' && __args[2] != null)) {
+    return utils.convReturnVertxGen(OAuth2Auth, JKeycloakAuth["create(io.vertx.core.Vertx,io.vertx.ext.auth.oauth2.OAuth2FlowType,io.vertx.core.json.JsonObject)"](__args[0]._jdel, io.vertx.ext.auth.oauth2.OAuth2FlowType.valueOf(__args[1]), utils.convParamJsonObject(__args[2])));
+  }else if (__args.length === 3 && typeof __args[0] === 'object' && __args[0]._jdel && (typeof __args[1] === 'object' && __args[1] != null) && (typeof __args[2] === 'object' && __args[2] != null)) {
+    return utils.convReturnVertxGen(OAuth2Auth, JKeycloakAuth["create(io.vertx.core.Vertx,io.vertx.core.json.JsonObject,io.vertx.core.http.HttpClientOptions)"](__args[0]._jdel, utils.convParamJsonObject(__args[1]), __args[2] != null ? new HttpClientOptions(new JsonObject(JSON.stringify(__args[2]))) : null));
+  }else if (__args.length === 4 && typeof __args[0] === 'object' && __args[0]._jdel && typeof __args[1] === 'string' && (typeof __args[2] === 'object' && __args[2] != null) && (typeof __args[3] === 'object' && __args[3] != null)) {
+    return utils.convReturnVertxGen(OAuth2Auth, JKeycloakAuth["create(io.vertx.core.Vertx,io.vertx.ext.auth.oauth2.OAuth2FlowType,io.vertx.core.json.JsonObject,io.vertx.core.http.HttpClientOptions)"](__args[0]._jdel, io.vertx.ext.auth.oauth2.OAuth2FlowType.valueOf(__args[1]), utils.convParamJsonObject(__args[2]), __args[3] != null ? new HttpClientOptions(new JsonObject(JSON.stringify(__args[3]))) : null));
+  } else throw new TypeError('function invoked with invalid arguments');
+};
+
+module.exports = KeycloakAuth;
