a3717bf1122b9b20fc3b2c8c9881acc072e52c6f
Julien Viet <julien@julienviet.com>
2017-03-02 17:45:31 +0100

    Use vertx-ext-parent 26

--- vertx-auth-oauth2/src/main/kotlin/io/vertx/kotlin/ext/auth/oauth2/OAuth2ClientOptions.kt
+++ vertx-auth-oauth2/src/main/kotlin/io/vertx/kotlin/ext/auth/oauth2/OAuth2ClientOptions.kt
@@ -51,6 +51,7 @@ import io.vertx.core.net.ProxyOptions
  * @param maxRedirects 
  * @param maxWaitQueueSize 
  * @param maxWebsocketFrameSize 
+ * @param maxWebsocketMessageSize 
  * @param metricsName 
  * @param openSslEngineOptions 
  * @param pemKeyCertOptions 
@@ -125,6 +126,7 @@ fun OAuth2ClientOptions(
   maxRedirects: Int? = null,
   maxWaitQueueSize: Int? = null,
   maxWebsocketFrameSize: Int? = null,
+  maxWebsocketMessageSize: Int? = null,
   metricsName: String? = null,
   openSslEngineOptions: io.vertx.core.net.OpenSSLEngineOptions? = null,
   pemKeyCertOptions: io.vertx.core.net.PemKeyCertOptions? = null,
@@ -272,6 +274,9 @@ fun OAuth2ClientOptions(
   }
   if (maxWebsocketFrameSize != null) {
     this.setMaxWebsocketFrameSize(maxWebsocketFrameSize)
+  }
+  if (maxWebsocketMessageSize != null) {
+    this.setMaxWebsocketMessageSize(maxWebsocketMessageSize)
   }
   if (metricsName != null) {
     this.setMetricsName(metricsName)
