050212ec3ef9c495b6d34210baba8394aec7fe3e
Neven Radovanovic <neven.radovanovic@gmail.com>
2017-03-17 10:51:38 +0100

    add a simple htpasswd provider #105 - fixing javadoc and comments

--- vertx-auth-htpasswd/src/main/java/io/vertx/ext/auth/htpasswd/digest/BCrypt.java
+++ vertx-auth-htpasswd/src/main/java/io/vertx/ext/auth/htpasswd/digest/BCrypt.java
@@ -1,25 +1,4 @@
-/***********************************************************************************************************************
- *
- * Copyright (C) 2013, 2014 by huanju (http://www.yy.com)
- * http://www.yy.com/
- *
- ***********************************************************************************************************************
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- *
- **********************************************************************************************************************/
 package io.vertx.ext.auth.htpasswd.digest;
-
-/**
- * @author jiangnan
- */
 
 import java.io.UnsupportedEncodingException;
 import java.security.SecureRandom;
@@ -498,7 +477,7 @@ public class BCrypt {
    * @param lr  an array containing the two 32-bit half blocks
    * @param off the position in the array of the blocks
    */
-  private final void encipher(int lr[], int off) {
+  private void encipher(int lr[], int off) {
     int i, n, l = lr[off], r = lr[off + 1];
 
     l ^= P[0];
