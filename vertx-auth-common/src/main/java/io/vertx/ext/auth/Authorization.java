7e1f92fda04fdecfb3c5f77d343a7626f54d9c89
stephane.bastian.dev <stephane.bastian.dev@gmail.com>
2019-10-10 12:56:21 +0200

    removed extra @GenIgnore from the file Authorization added copyright headers

--- vertx-auth-common/src/main/java/io/vertx/ext/auth/Authorization.java
+++ vertx-auth-common/src/main/java/io/vertx/ext/auth/Authorization.java
@@ -1,6 +1,17 @@
+/********************************************************************************
+ * Copyright (c) 2019 Stephane Bastian
+ *
+ * This program and the accompanying materials are made available under the 2
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 3
+ *
+ * Contributors: 4
+ *   Stephane Bastian - initial API and implementation
+ ********************************************************************************/
 package io.vertx.ext.auth;
 
-import io.vertx.codegen.annotations.GenIgnore;
 import io.vertx.codegen.annotations.VertxGen;
 
 /**
@@ -37,7 +48,6 @@ public interface Authorization {
    * @param context
    * @return
    */
-  @GenIgnore
   boolean match(AuthorizationContext context);
 
   /**
@@ -52,7 +62,6 @@ public interface Authorization {
    * @param authorization
    * @return
    */
-  @GenIgnore
   boolean verify(Authorization authorization);
 
 }
--- vertx-auth-common/src/main/java/io/vertx/ext/auth/impl/AndAuthorizationImpl.java
+++ vertx-auth-common/src/main/java/io/vertx/ext/auth/impl/AndAuthorizationImpl.java
@@ -1,3 +1,15 @@
+/********************************************************************************
+ * Copyright (c) 2019 Stephane Bastian
+ *
+ * This program and the accompanying materials are made available under the 2
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 3
+ *
+ * Contributors: 4
+ *   Stephane Bastian - initial API and implementation
+ ********************************************************************************/
 package io.vertx.ext.auth.impl;
 
 import java.util.ArrayList;
