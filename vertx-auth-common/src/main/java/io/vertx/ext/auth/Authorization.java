3300821f40d1acacc97aeb3cc5c4f5bdd68269f7
stephane.bastian.dev <stephane.bastian.dev@gmail.com>
2019-10-10 11:07:38 +0200

    fixed indentation and javadoc comments that are not aligned

--- vertx-auth-common/src/main/java/io/vertx/ext/auth/Authorization.java
+++ vertx-auth-common/src/main/java/io/vertx/ext/auth/Authorization.java
@@ -4,17 +4,25 @@ import io.vertx.codegen.annotations.GenIgnore;
 import io.vertx.codegen.annotations.VertxGen;
 
 /**
- * Interface representing any kind of authorization such as: - Role based
- * authorization - Permission based authorization - Logical authorization (AND,
- * OR, NOT) - Time based authorization (ie: allow access the last 5 days of the
- * month, from 8am till 10am, etc.) - Context based authorization (ie: allow
- * access if the ip address is 'xxx.xxx.xxx.xxx') - Custom based authorization
- * (ie: based on a script or hard-coded code specific to an application) - etc.
- * 
- * The following implementations are provided out of the box: -
- * {@link AndAuthorization} - {@link NotAuthorization} - {@link OrAuthorization}
- * - {@link PermissionBasedAuthorization} - {@link RoleBasedAuthorization} -
- * {@link WildcardPermissionBasedAuthorization}
+ * Interface representing any kind of authorization such as: 
+ * <ul>
+ *   <li>Role based authorization
+ *   <li>Permission based authorization
+ *   <li>Logical authorization (AND, OR, NOT) 
+ *   <li>Time based authorization (ie: allow access the last 5 days of the month, from 8am till 10am, etc.)
+ *   <li>Context based authorization (ie: allow access if the ip address is 'xxx.xxx.xxx.xxx')
+ *   <li>Custom based authorization (ie: based on a script or hard-coded code specific to an application)
+ *   <li>etc.
+ * </ul>
+ * The following implementations are provided out of the box:
+ * <ul>
+ *   <li>{@link AndAuthorization}
+ *   <li>{@link NotAuthorization}
+ *   <li>{@link OrAuthorization}
+ *   <li>{@link PermissionBasedAuthorization}
+ *   <li>{@link RoleBasedAuthorization}
+ *   <li>{@link WildcardPermissionBasedAuthorization}
+ * </ul>
  * 
  * @author <a href="mail://stephane.bastian.dev@gmail.com">Stephane Bastian</a>
  *
@@ -33,12 +41,12 @@ public interface Authorization {
   boolean match(AuthorizationContext context);
 
   /**
-   * 
    * this method verifies whether or not the authorization implies the specified
-   * authorization. Note that it doesn't always mean an exact match. For instance,
+   * authorization. 
+   * </br>Note that it doesn't always mean an exact match. For instance,
    * in the case of a {@link WildcardPermissionBasedAuthorization}, this method
-   * may return true even if the permissions are different =>
-   * WildcardPermissionBasedAuthorization.create('*').implies(WildcardPermissionBasedAuthorization.create('anypermission'))
+   * may return true even if the permissions are different
+   * </br>WildcardPermissionBasedAuthorization.create('*').implies(WildcardPermissionBasedAuthorization.create('anypermission')) 
    * would return true
    * 
    * @param authorization
--- vertx-auth-common/src/test/java/io/vertx/ext/auth/TestUtils.java
+++ vertx-auth-common/src/test/java/io/vertx/ext/auth/TestUtils.java
@@ -8,19 +8,20 @@ import io.vertx.core.json.JsonObject;
 
 public class TestUtils {
 
-	public final static <T> void testJsonCodec(T authorization, Function<T, JsonObject> toJsonConverter, Function<JsonObject, T> fromJsonConverter) {
-		Assert.assertNotNull(authorization);
-		JsonObject json = toJsonConverter.apply(authorization);
-		T otherAuthorization = fromJsonConverter.apply(json);
-		Assert.assertEquals(authorization, otherAuthorization);
-	}
+  public final static <T> void testJsonCodec(T authorization, Function<T, JsonObject> toJsonConverter,
+      Function<JsonObject, T> fromJsonConverter) {
+    Assert.assertNotNull(authorization);
+    JsonObject json = toJsonConverter.apply(authorization);
+    T otherAuthorization = fromJsonConverter.apply(json);
+    Assert.assertEquals(authorization, otherAuthorization);
+  }
 
-	public final static AuthorizationContext getTestAuthorizationContext() {
-		return getTestAuthorizationContext(User.create(new JsonObject().put("username", "dummy user")));
-	}
+  public final static AuthorizationContext getTestAuthorizationContext() {
+    return getTestAuthorizationContext(User.create(new JsonObject().put("username", "dummy user")));
+  }
 
-	public final static AuthorizationContext getTestAuthorizationContext(User user) {
-		return null;
-	}
+  public final static AuthorizationContext getTestAuthorizationContext(User user) {
+    return null;
+  }
 
 }
