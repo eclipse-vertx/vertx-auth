5bf60d8da3d5ec119f835d175591f4af6eb1a537
Frederic Giloux <fgiloux@redhat.com>
2019-12-10 08:30:33 +0100

    use the right codec

--- vertx-auth-oauth2/src/main/java/io/vertx/ext/auth/oauth2/KeycloakHelper.java
+++ vertx-auth-oauth2/src/main/java/io/vertx/ext/auth/oauth2/KeycloakHelper.java
@@ -126,7 +126,7 @@ public interface KeycloakHelper {
       throw new IllegalArgumentException("Parsing error");
     }
     try {
-      String decoded = new String(Base64.getDecoder().decode(parts[1]), "UTF-8");
+      String decoded = new String(Base64.getUrlDecoder().decode(parts[1]), "UTF-8");
       return new JsonObject(decoded); // get "payload" part
     } catch (Exception e) {
       e.printStackTrace();
