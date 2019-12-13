d763e466f3c55c9fdf21f21f4cd8f4d7e329b7f8
Paulo Lopes <pmlopes@gmail.com>
2019-12-13 14:40:37 +0100

    fix typpo

--- vertx-auth-sql/src/main/resources/sql-auth-ddl.sql
+++ vertx-auth-sql/src/main/resources/sql-auth-ddl.sql
@@ -17,4 +17,4 @@ ALTER TABLE users ADD CONSTRAINT `pk_username` PRIMARY KEY (username);
 ALTER TABLE users_roles ADD CONSTRAINT `pk_users_roles` PRIMARY KEY (username, role);
 ALTER TABLE roles_perms ADD CONSTRAINT `pk_roles_perms` PRIMARY KEY (role, perm);
 
-ALTER TABLE users_roles ADD CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES user(username);
+ALTER TABLE users_roles ADD CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users(username);
