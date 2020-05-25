01d2b2b9612a1c05a9d17691568179d27ffe4c85
Julien Viet <julien@julienviet.com>
2020-05-25 09:47:32 +0200

    Rename SQL auth artifact to SQL

---
+++ vertx-auth-sql-client/src/main/java/io/vertx/ext/auth/sqlclient/SqlAuthenticationOptions.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2015 Red Hat, Inc.
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *
+ *  The Eclipse Public License is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  The Apache License v2.0 is available at
+ *  http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  You may elect to redistribute this code under either of these licenses.
+ */
+package io.vertx.ext.auth.sqlclient;
+
+import io.vertx.codegen.annotations.DataObject;
+import io.vertx.codegen.annotations.Fluent;
+import io.vertx.core.json.JsonObject;
+
+/**
+ * Options configuring JDBC authentication.
+ *
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+@DataObject(generateConverter = true)
+public class SqlAuthenticationOptions {
+
+  /**
+   * The default query to be used for authentication
+   */
+  private final static String DEFAULT_AUTHENTICATE_QUERY = "SELECT password FROM users WHERE username = ?";
+
+  private String authenticationQuery;
+
+  public SqlAuthenticationOptions() {
+    this.authenticationQuery = DEFAULT_AUTHENTICATE_QUERY;
+  }
+
+  public SqlAuthenticationOptions(JsonObject json) {
+    this();
+    SqlAuthenticationOptionsConverter.fromJson(json, this);
+  }
+
+  public String getAuthenticationQuery() {
+    return authenticationQuery;
+  }
+
+  /**
+   * Set the authentication query to use. Use this if you want to override the
+   * default authentication query.
+   *
+   * @param authenticationQuery the authentication query
+   * @return a reference to this, so the API can be used fluently
+   */
+  @Fluent
+  public SqlAuthenticationOptions setAuthenticationQuery(String authenticationQuery) {
+    this.authenticationQuery = authenticationQuery;
+    return this;
+  }
+
+  public JsonObject toJson() {
+    JsonObject json = new JsonObject();
+    SqlAuthenticationOptionsConverter.toJson(this, json);
+    return json;
+  }
+}
