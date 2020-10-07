b6cdfa93254297a86aa5f12975933400347dcc65
Paulo Lopes <pmlopes@gmail.com>
2020-10-07 16:23:51 +0200

    Adding basic test
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- vertx-auth-webauthn/src/main/java/io/vertx/ext/auth/webauthn/impl/attestation/AppleAttestation.java
+++ vertx-auth-webauthn/src/main/java/io/vertx/ext/auth/webauthn/impl/attestation/AppleAttestation.java
@@ -27,6 +27,7 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.*;
 
+import static io.vertx.ext.auth.webauthn.impl.attestation.ASN1.*;
 import static io.vertx.ext.auth.webauthn.impl.attestation.Attestation.*;
 
 /**
@@ -44,20 +45,18 @@ public class AppleAttestation implements Attestation {
    * Valid until 03/14/2045 @ 5:00 PM PST
    */
   private static final String APPLE_WEBAUTHN_ROOT_CA =
-    "MIICizCCAjKgAwIBAgIJAKIFntEOQ1tXMAoGCCqGSM49BAMCMIGYMQswCQYDVQQG" +
-      "EwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNTW91bnRhaW4gVmll" +
-      "dzEVMBMGA1UECgwMR29vZ2xlLCBJbmMuMRAwDgYDVQQLDAdBbmRyb2lkMTMwMQYD" +
-      "VQQDDCpBbmRyb2lkIEtleXN0b3JlIFNvZnR3YXJlIEF0dGVzdGF0aW9uIFJvb3Qw" +
-      "HhcNMTYwMTExMDA0MzUwWhcNMzYwMTA2MDA0MzUwWjCBmDELMAkGA1UEBhMCVVMx" +
-      "EzARBgNVBAgMCkNhbGlmb3JuaWExFjAUBgNVBAcMDU1vdW50YWluIFZpZXcxFTAT" +
-      "BgNVBAoMDEdvb2dsZSwgSW5jLjEQMA4GA1UECwwHQW5kcm9pZDEzMDEGA1UEAwwq" +
-      "QW5kcm9pZCBLZXlzdG9yZSBTb2Z0d2FyZSBBdHRlc3RhdGlvbiBSb290MFkwEwYH" +
-      "KoZIzj0CAQYIKoZIzj0DAQcDQgAE7l1ex-HA220Dpn7mthvsTWpdamguD_9_SQ59" +
-      "dx9EIm29sa_6FsvHrcV30lacqrewLVQBXT5DKyqO107sSHVBpKNjMGEwHQYDVR0O" +
-      "BBYEFMit6XdMRcOjzw0WEOR5QzohWjDPMB8GA1UdIwQYMBaAFMit6XdMRcOjzw0W" +
-      "EOR5QzohWjDPMA8GA1UdEwEB_wQFMAMBAf8wDgYDVR0PAQH_BAQDAgKEMAoGCCqG" +
-      "SM49BAMCA0cAMEQCIDUho--LNEYenNVg8x1YiSBq3KNlQfYNns6KGYxmSGB7AiBN" +
-      "C_NR2TB8fVvaNTQdqEcbY6WFZTytTySn502vQX3xvw";
+      "MIICEjCCAZmgAwIBAgIQaB0BbHo84wIlpQGUKEdXcTAKBggqhkjOPQQDAzBLMR8w" +
+      "HQYDVQQDDBZBcHBsZSBXZWJBdXRobiBSb290IENBMRMwEQYDVQQKDApBcHBsZSBJ" +
+      "bmMuMRMwEQYDVQQIDApDYWxpZm9ybmlhMB4XDTIwMDMxODE4MjEzMloXDTQ1MDMx" +
+      "NTAwMDAwMFowSzEfMB0GA1UEAwwWQXBwbGUgV2ViQXV0aG4gUm9vdCBDQTETMBEG" +
+      "A1UECgwKQXBwbGUgSW5jLjETMBEGA1UECAwKQ2FsaWZvcm5pYTB2MBAGByqGSM49" +
+      "AgEGBSuBBAAiA2IABCJCQ2pTVhzjl4Wo6IhHtMSAzO2cv+H9DQKev3//fG59G11k" +
+      "xu9eI0/7o6V5uShBpe1u6l6mS19S1FEh6yGljnZAJ+2GNP1mi/YK2kSXIuTHjxA/" +
+      "pcoRf7XkOtO4o1qlcaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUJtdk" +
+      "2cV4wlpn0afeaxLQG2PxxtcwDgYDVR0PAQH/BAQDAgEGMAoGCCqGSM49BAMDA2cA" +
+      "MGQCMFrZ+9DsJ1PW9hfNdBywZDsWDbWFp28it1d/5w2RPkRX3Bbn/UbDTNLx7Jr3" +
+      "jAGGiQIwHFj+dJZYUJR786osByBelJYsVZd2GbHQu209b5RCmGQ21gpSAk9QZW4B" +
+      "1bWeT0vT";
 
   public AppleAttestation() {
   }
@@ -74,7 +73,6 @@ public class AppleAttestation implements Attestation {
 
       // Check attStmt and it contains “x5c” then its a FULL attestation.
       JsonObject attStmt = attestation.getJsonObject("attStmt");
-      byte[] signature = attStmt.getBinary("sig");
 
       if (!attStmt.containsKey("x5c")) {
         throw new AttestationException("No attestation x5c");
@@ -103,7 +101,16 @@ public class AppleAttestation implements Attestation {
       // 4. Verify |nonce| matches the value of the extension with OID ( 1.2.840.113635.100.8.2 ) in |credCert|.
       final X509Certificate credCert = certChain.get(0);
       byte[] appleExtension = credCert.getExtensionValue("1.2.840.113635.100.8.2");
-      if (!MessageDigest.isEqual(nonce, appleExtension)) {
+      ASN1.ASN extension = ASN1.parseASN1(appleExtension);
+      if (extension.tag.type != OCTET_STRING) {
+        throw new AttestationException("1.2.840.113635.100.8.2 Extension is not an ASN.1 OCTET string!");
+      }
+      // parse the octet as ASN.1 and expect it to se a sequence
+      extension = parseASN1(extension.binary(0));
+      if (extension.tag.type != SEQUENCE) {
+        throw new AttestationException("1.2.840.113635.100.8.2 Extension is not an ASN.1 SEQUENCE!");
+      }
+      if (!MessageDigest.isEqual(nonce, extension.object(0).object(0).binary(0))) {
         throw new AttestationException("Certificate 1.2.840.113635.100.8.2 extension does not match nonce");
       }
 
