5c295df010a49035cba5a91679dc59af3850fcf4
Julien Viet <julien@julienviet.com>
2021-10-28 09:52:48 +0200

    Releasing 4.2.1-SNAPSHOT

--- vertx-auth-common/src/main/generated/io/vertx/ext/auth/PubSecKeyOptionsConverter.java
+++ vertx-auth-common/src/main/generated/io/vertx/ext/auth/PubSecKeyOptionsConverter.java
@@ -5,6 +5,7 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.impl.JsonUtil;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 
 /**
  * Converter and mapper for {@link io.vertx.ext.auth.PubSecKeyOptions}.
@@ -12,6 +13,9 @@ import java.time.format.DateTimeFormatter;
  */
 public class PubSecKeyOptionsConverter {
 
+
+  private static final Base64.Decoder BASE64_DECODER = JsonUtil.BASE64_DECODER;
+  private static final Base64.Encoder BASE64_ENCODER = JsonUtil.BASE64_ENCODER;
 
   public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, PubSecKeyOptions obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
@@ -23,7 +27,7 @@ public class PubSecKeyOptionsConverter {
           break;
         case "buffer":
           if (member.getValue() instanceof String) {
-            obj.setBuffer(io.vertx.core.buffer.Buffer.buffer(JsonUtil.BASE64_DECODER.decode((String)member.getValue())));
+            obj.setBuffer(io.vertx.core.buffer.Buffer.buffer(BASE64_DECODER.decode((String)member.getValue())));
           }
           break;
         case "certificate":
@@ -64,7 +68,7 @@ public class PubSecKeyOptionsConverter {
       json.put("algorithm", obj.getAlgorithm());
     }
     if (obj.getBuffer() != null) {
-      json.put("buffer", JsonUtil.BASE64_ENCODER.encodeToString(obj.getBuffer().getBytes()));
+      json.put("buffer", BASE64_ENCODER.encodeToString(obj.getBuffer().getBytes()));
     }
     json.put("certificate", obj.isCertificate());
     if (obj.getId() != null) {
