15501c4af9a5074babb396d00e48a5b0754279dd
Paulo Lopes <pmlopes@gmail.com>
2023-03-09 13:33:49 +0100

    Adding missing javadoc
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- vertx-auth-webauthn/src/main/java/io/vertx/ext/auth/webauthn/ResidentKey.java
+++ vertx-auth-webauthn/src/main/java/io/vertx/ext/auth/webauthn/ResidentKey.java
@@ -1,9 +1,30 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *
+ *  The Eclipse Public License is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  The Apache License v2.0 is available at
+ *  http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  You may elect to redistribute this code under either of these licenses.
+ */
 package io.vertx.ext.auth.webauthn;
 
 import io.vertx.codegen.annotations.GenIgnore;
 import io.vertx.codegen.annotations.Nullable;
 import io.vertx.codegen.annotations.VertxGen;
 
+/**
+ * ResidentKey
+ * https://www.w3.org/TR/webauthn-2/#dictdef-authenticatorselectioncriteria
+ *
+ * This enum is used to specify the desired behaviour for resident keys with the authenticator.
+ */
 @VertxGen
 public enum ResidentKey {
   DISCOURAGED("discouraged"),
