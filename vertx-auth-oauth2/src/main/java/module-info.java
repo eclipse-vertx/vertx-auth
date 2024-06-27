687bdaafaa303199743ad9f14938b12648455ffc
Julien Viet <julien@julienviet.com>
2024-06-27 09:32:49 +0200

    JPMS support improvements

--- vertx-auth-oauth2/src/main/java/module-info.java
+++ vertx-auth-oauth2/src/main/java/module-info.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright (c) 2011-2024 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
 module io.vertx.auth.oauth2 {
 
   requires transitive io.vertx.auth.common;
   requires io.vertx.core.logging;
+
+  exports io.vertx.ext.auth.oauth2;
+  exports io.vertx.ext.auth.oauth2.authorization;
+  exports io.vertx.ext.auth.oauth2.providers;
 
   requires static io.vertx.codegen.api;
   requires static io.vertx.codegen.json;
