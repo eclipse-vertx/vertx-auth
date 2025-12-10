a301c33b07227267088a3b184664bb63ca890696
Lazar Bulic <lazarbulic@gmail.com>
2025-12-10 17:27:44 +0100

    Only log Unsupported JWK when debug is enabled

--- vertx-auth-oauth2/src/main/java/io/vertx/ext/auth/oauth2/impl/OAuth2AuthProviderImpl.java
+++ vertx-auth-oauth2/src/main/java/io/vertx/ext/auth/oauth2/impl/OAuth2AuthProviderImpl.java
@@ -124,7 +124,7 @@ public class OAuth2AuthProviderImpl implements OAuth2Auth, Closeable {
             try {
               jwt.addJWK(new JWK((JsonObject) key));
             } catch (Exception e) {
-              if(LOG.isDebugEnabled()) {
+              if (LOG.isDebugEnabled()) {
                 LOG.warn("Unsupported JWK", e);
               }
             }
