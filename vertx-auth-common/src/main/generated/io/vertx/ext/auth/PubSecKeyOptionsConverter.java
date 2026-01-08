35577770aa312c3e9065aa67ee6861425c1599f3
Thomas Segismont <tsegismont@gmail.com>
2026-01-08 11:29:38 +0100

    Revert "Update generated files"
    This reverts commit 79deecd403105abae85de19f52932a181b5cfb2f.

---
+++ vertx-auth-common/src/main/generated/io/vertx/ext/auth/PubSecKeyOptionsConverter.java
@@ -0,0 +1,49 @@
+package io.vertx.ext.auth;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+
+/**
+ * Converter and mapper for {@link io.vertx.ext.auth.PubSecKeyOptions}.
+ * NOTE: This class has been automatically generated from the {@link io.vertx.ext.auth.PubSecKeyOptions} original class using Vert.x codegen.
+ */
+public class PubSecKeyOptionsConverter {
+
+   static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, PubSecKeyOptions obj) {
+    for (java.util.Map.Entry<String, Object> member : json) {
+      switch (member.getKey()) {
+        case "algorithm":
+          if (member.getValue() instanceof String) {
+            obj.setAlgorithm((String)member.getValue());
+          }
+          break;
+        case "buffer":
+          if (member.getValue() instanceof String) {
+            obj.setBuffer(io.vertx.core.buffer.Buffer.fromJson((String)member.getValue()));
+          }
+          break;
+        case "id":
+          if (member.getValue() instanceof String) {
+            obj.setId((String)member.getValue());
+          }
+          break;
+      }
+    }
+  }
+
+   static void toJson(PubSecKeyOptions obj, JsonObject json) {
+    toJson(obj, json.getMap());
+  }
+
+   static void toJson(PubSecKeyOptions obj, java.util.Map<String, Object> json) {
+    if (obj.getAlgorithm() != null) {
+      json.put("algorithm", obj.getAlgorithm());
+    }
+    if (obj.getBuffer() != null) {
+      json.put("buffer", obj.getBuffer().toJson());
+    }
+    if (obj.getId() != null) {
+      json.put("id", obj.getId());
+    }
+  }
+}
---
+++ vertx-auth-common/src/main/generated/io/vertx/ext/auth/authentication/UsernamePasswordCredentialsConverter.java
@@ -0,0 +1,41 @@
+package io.vertx.ext.auth.authentication;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+
+/**
+ * Converter and mapper for {@link io.vertx.ext.auth.authentication.UsernamePasswordCredentials}.
+ * NOTE: This class has been automatically generated from the {@link io.vertx.ext.auth.authentication.UsernamePasswordCredentials} original class using Vert.x codegen.
+ */
+public class UsernamePasswordCredentialsConverter {
+
+   static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, UsernamePasswordCredentials obj) {
+    for (java.util.Map.Entry<String, Object> member : json) {
+      switch (member.getKey()) {
+        case "password":
+          if (member.getValue() instanceof String) {
+            obj.setPassword((String)member.getValue());
+          }
+          break;
+        case "username":
+          if (member.getValue() instanceof String) {
+            obj.setUsername((String)member.getValue());
+          }
+          break;
+      }
+    }
+  }
+
+   static void toJson(UsernamePasswordCredentials obj, JsonObject json) {
+    toJson(obj, json.getMap());
+  }
+
+   static void toJson(UsernamePasswordCredentials obj, java.util.Map<String, Object> json) {
+    if (obj.getPassword() != null) {
+      json.put("password", obj.getPassword());
+    }
+    if (obj.getUsername() != null) {
+      json.put("username", obj.getUsername());
+    }
+  }
+}
