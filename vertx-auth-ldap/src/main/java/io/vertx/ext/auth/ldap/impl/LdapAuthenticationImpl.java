7f878d3f2de6a5374150c563783cc84ff94f16df
Thomas Segismont <tsegismont@gmail.com>
2026-03-02 14:54:44 +0100

    Erroneous SPDX statements in multiple files
    Closes #744
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-auth-ldap/src/main/java/io/vertx/ext/auth/ldap/impl/LdapAuthenticationImpl.java
+++ vertx-auth-ldap/src/main/java/io/vertx/ext/auth/ldap/impl/LdapAuthenticationImpl.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
  *
- * SPDX-License-Identifier: EPL-2.0 3
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors: 4
  *   Stephane Bastian - initial API and implementation
